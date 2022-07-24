cb13a02f3571441390c4f70485c4144d0d9fcca2
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-07-24 12:19:43 -0600

    URL request successful/error response

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,29 @@ var prevCitySearched = ""
 var cityWeather = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=33a442ce0b1dad52f9352616c57d9d69&units=imperial";
     
+    // URL request
+    fetch(apiUrl)
+    .then(function(response) {
+        
+        // If request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayWeather(data);
+            });
+
+            // If request fails
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+
+    // Sends alert if no response from open weather
+    .catch(function(error) {
+        alert("Unable to connect to openweathermap.org");
+    })
+};
+
+// Function for city search form submit
+var citySearch = function(event) {
+    event.preventDefault();
 }
