a64799550cfc96d8d746d6d3e683c9f40fd7656d
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-07-24 15:57:09 -0600

    Generated main weather card info

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,6 @@ var prevCitySearched = ""
 // API call to openweathermap.org
 var cityWeather = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=33a442ce0b1dad52f9352616c57d9d69&units=imperial";
-    
     // URL request
     fetch(apiUrl)
     .then(function(response) {
@@ -13,6 +12,8 @@ var cityWeather = function(city) {
         // If request was successful
         if (response.ok) {
             response.json().then(function(data) {
+                console.log(data);
+                console.log(data.weather);
                 displayWeather(data);
             });
 
@@ -31,4 +32,29 @@ var cityWeather = function(city) {
 // Function for city search form submit
 var citySearch = function(event) {
     event.preventDefault();
+
+    // Get value from cityname id element
+    var cityName = $("#cityname").val().trim()
+    // sends value to cityWeather function
+    if (cityName) {
+        cityWeather(cityName);
+
+        // Clears search input
+        $("#cityname").val("");
+
+        // Sends alert if nothing was inputed
+    } else {
+        alert("Enter a city name");
+    }
+};
+
+// Function to display the data from openweathermap.org
+var displayWeather = function(weatherData) {
+    $("#main-city-name").text(`${weatherData.name}`);
+    $("#main-city-temp").text(`Temp: ${weatherData.main.temp}` + "°F");
+    $("#main-city-humid").text(`Humidity: ${weatherData.main.humidity}` + "%");
+    $("#main-city-wind").text(`Wind Speed: ${weatherData.wind.speed}` + " mph");
 }
+
+// Searches city weather when search button is clicked
+$("#search").on("click", citySearch);
